27f1e1657a62a477642f278fc704e6fdc598f24a
Thomas Frank <thomas.frank@geo.uu.se>
2023-05-30 14:09:42 +0200

    'ís_odd' add doc

---
+++ is_odd.py
@@ -0,0 +1,22 @@
+def is_odd(x):
+    """
+    Checks if input is an odd integer
+
+    Returns True if input is an odd integer
+    Returns False if input is not an odd integer
+    Raises Error if input is not an integer
+    """
+
+
+
+
+
+
+
+
+
+
+
+
+
+assert is_odd.__doc__ is not None
